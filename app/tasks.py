a91c40eb05044b2d7e22d4ad7d7eeb0ce38377d6
swcasimiro <comradecasi@gmail.com>
2024-09-23 13:53:46 +0300

    fix requirements + add celery

--- app/tasks.py
+++ app/tasks.py
@@ -1,4 +1,7 @@
 from celery import Celery
+import requests
 
+base_url = "https://kinopoiskapiunofficial.tech/api/v2.2/films/1186526"
+appid = {'X-API-KEY': 'f98a5461-d00d-4929-9a5c-e441318daba7'}
 
 
