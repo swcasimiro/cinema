a9b266e10e6e47de840f3115717e0f8627035447
swcasimiro <comradecasi@gmail.com>
2024-09-24 16:38:19 +0300

    add celery

--- app/app/settings.py
+++ app/app/settings.py
@@ -136,3 +136,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
